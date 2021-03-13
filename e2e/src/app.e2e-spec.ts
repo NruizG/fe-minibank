2de08a4bc1b76ebf0fa463d28e20c2500c76e890
Nicolas Ruiz Gallardo <nicolasruiz81@gmail.com>
2021-03-13 15:57:36 -0300

    ✨ Changes package name and downgraded Angular version

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('mini-bank app is running!');
+    expect(await page.getTitleText()).toEqual('fe-mini-bank app is running!');
   });
 
   afterEach(async () => {

0e69ecf1aead2a9ba80168732c022a78f2b685e5
Nicolas Ruiz Gallardo <nicolasruiz81@gmail.com>
2021-03-15 20:58:41 -0300

    🎨 Added production ms urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_AUTHENTICATION: 'https://ms-authentication-phz7n2vvva-rj.a.run.app',
+  API_PLATFORM: 'https://ms-platform-phz7n2vvva-rj.a.run.app'
 };
